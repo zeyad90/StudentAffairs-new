24b54ce3a10596267250a2778347dcba7bee05dc
unknown <zeyadnaji90@gmail.com>
2023-03-08 17:29:39 +0300

    forth commit

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -173,4 +173,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-django_heroku.settings(locals())
+django_heroku.settings(locals()) 
